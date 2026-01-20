4aae2aa18a9190a7da1840722a83138d3c30e552
Mariia Zueva <maryzueva@gmail.com>
2026-01-20 10:18:40 +0100

    MILAB-5339 Label migration

--- ui/src/app.ts
+++ ui/src/app.ts
@@ -1,6 +1,7 @@
 import { model } from '@platforma-open/milaboratories.import-vdj.model';
 import { defineApp } from '@platforma-sdk/ui-vue';
 import MainPage from './pages/MainPage.vue';
+import { watch } from 'vue';
 
 export const sdkPlugin = defineApp(model, () => {
   return {
@@ -11,3 +12,12 @@ export const sdkPlugin = defineApp(model, () => {
 });
 
 export const useApp = sdkPlugin.useApp;
+
+// Make sure labels are initialized
+const unwatch = watch(sdkPlugin, ({ loaded }) => {
+  if (!loaded) return;
+  const app = useApp();
+  app.model.args.customBlockLabel ??= '';
+  app.model.args.defaultBlockLabel ??= 'Select Clonotype Definition';
+  unwatch();
+});
